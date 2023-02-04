6ad06eddcb50ebce156f9a8da5850c2d970673c8
Dmitriy Yastrebov <somewonderfulguy@gmail.com>
2023-02-04 22:26:46 +0200

    merging - wip

--- packages/library/config-overrides.js
+++ packages/library/config-overrides.js
@@ -1,24 +1,24 @@
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 
-const deps = require("./package.json").dependencies;
+const deps = require('./package.json').dependencies
 module.exports = function override(config) {
-  config.output.publicPath = "auto";
+  config.output.publicPath = 'auto'
 
   if (!config.plugins) {
-    config.plugins = [];
+    config.plugins = []
   }
 
   config.plugins.unshift(
     new ModuleFederationPlugin({
-      name: "shared",
-      filename: "shared.js",
+      name: 'library',
+      filename: 'library.js',
       remotes: {},
       exposes: {
         // keep in alphabetical order
-        "./Block": "./src/components/Block",
-        "./Button": "./src/components/formLike/Button",
-        "./usePrevious": "./src/hooks/usePrevious",
-        "./useResizeObserver": "./src/hooks/useResizeObserver",
+        './Block': './src/components/Block',
+        './Button': './src/components/formLike/Button',
+        './usePrevious': './src/hooks/usePrevious',
+        './useResizeObserver': './src/hooks/useResizeObserver',
       },
       shared: {
         ...deps,
@@ -26,12 +26,12 @@ module.exports = function override(config) {
           singleton: true,
           requiredVersion: deps.react,
         },
-        "react-dom": {
+        'react-dom': {
           singleton: true,
-          requiredVersion: deps["react-dom"],
+          requiredVersion: deps['react-dom'],
         },
       },
     })
-  );
-  return config;
-};
+  )
+  return config
+}
--- packages/sub-application/config-overrides.js
+++ packages/sub-application/config-overrides.js
@@ -1,20 +1,20 @@
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 
-const deps = require("./package.json").dependencies;
+const deps = require('./package.json').dependencies
 module.exports = function override(config) {
-  config.output.publicPath = "auto";
+  config.output.publicPath = 'auto'
 
   if (!config.plugins) {
-    config.plugins = [];
+    config.plugins = []
   }
 
   config.plugins.unshift(
     new ModuleFederationPlugin({
-      name: "sub_application",
-      filename: "sub-application.js",
+      name: 'sub_application',
+      filename: 'sub-application.js',
       remotes: {},
       exposes: {
-        "./app": "./src/App.tsx",
+        './app': './src/App.tsx',
       },
       shared: {
         ...deps,
@@ -22,12 +22,12 @@ module.exports = function override(config) {
           singleton: true,
           requiredVersion: deps.react,
         },
-        "react-dom": {
+        'react-dom': {
           singleton: true,
-          requiredVersion: deps["react-dom"],
+          requiredVersion: deps['react-dom'],
         },
       },
     })
-  );
-  return config;
-};
+  )
+  return config
+}
