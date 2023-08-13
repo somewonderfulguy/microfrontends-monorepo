4f99e358e14146ad4f2800524034bdfb31102986
Dmitriy Yastrebov <somewonderfulguy@gmail.com>
2023-08-13 14:18:09 +0300

    upgrade storybook, add dummy select for changing storybook's theme

--- packages/library/.storybook/preview.tsx
+++ packages/library/.storybook/preview.tsx
@@ -16,7 +16,8 @@ const svgSharedProps2 = {
   xmlns: 'http://www.w3.org/2000/svg',
   viewBox: '0 0 16 16',
   fill: 'currentColor',
-  strokeWidth: '0'
+  strokeWidth: '0',
+  role: 'img'
 }
 
 const svgPrioritySharedProps = {
@@ -170,6 +171,39 @@ const multiselect: Addon = {
             title: 'Ukrainian',
             value: 'ua',
             left: '🇺🇦'
+          }
+        ]
+      }
+    ]
+  },
+  themeStorybook: {
+    icon: (
+      <svg {...svgPrioritySharedProps} fill="currentColor">
+        <path d="M16.71.243l-.12 2.71a.18.18 0 00.29.15l1.06-.8.9.7a.18.18 0 00.28-.14l-.1-2.76 1.33-.1a1.2 1.2 0 011.279 1.2v21.596a1.2 1.2 0 01-1.26 1.2l-16.096-.72a1.2 1.2 0 01-1.15-1.16l-.75-19.797a1.2 1.2 0 011.13-1.27L16.7.222zM13.64 9.3c0 .47 3.16.24 3.59-.08 0-3.2-1.72-4.89-4.859-4.89-3.15 0-4.899 1.72-4.899 4.29 0 4.45 5.999 4.53 5.999 6.959 0 .7-.32 1.1-1.05 1.1-.96 0-1.35-.49-1.3-2.16 0-.36-3.649-.48-3.769 0-.27 4.03 2.23 5.2 5.099 5.2 2.79 0 4.969-1.49 4.969-4.18 0-4.77-6.099-4.64-6.099-6.999 0-.97.72-1.1 1.13-1.1.45 0 1.25.07 1.19 1.87z" />
+      </svg>
+    ),
+    description: 'Change theme of Storybook (outer shell)',
+    elements: [
+      {
+        type: 'singleSelect',
+        title: 'Storybook theme shell',
+        queryKey: 'themeStorybook',
+        defaultValue: 'yellow',
+        options: [
+          {
+            title: 'Yellow',
+            value: 'yellow',
+            left: '🟡'
+          },
+          {
+            title: 'Dark Red',
+            value: 'darkRed',
+            left: '🔴'
+          },
+          {
+            title: 'Dark',
+            value: 'dark',
+            left: '⚫'
           }
         ]
       }
