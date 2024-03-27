cf4cc147617ff8b77ee73ee58cc5e0b811747fab
Dmitriy Yastrebov <somewonderfulguy@gmail.com>
2024-03-27 11:31:25 +0200

    add generateContext fn

---
+++ packages/library/src/utils/generateContext.tsx
@@ -0,0 +1,93 @@
+import {
+  useRef,
+  createContext,
+  useContext,
+  useCallback,
+  useSyncExternalStore,
+  ReactNode
+} from 'react'
+
+const generateContext = <Store,>(initialState: Store) => {
+  const useStoreData = (): {
+    get: () => Store
+    set: (value: Partial<Store>) => void
+    subscribe: (callback: () => void) => () => void
+  } => {
+    const store = useRef(initialState)
+
+    const get = useCallback(() => store.current, [])
+
+    const subscribers = useRef(new Set<() => void>())
+
+    const set = useCallback((value: Partial<Store>) => {
+      store.current = { ...store.current, ...value }
+      subscribers.current.forEach((callback) => callback())
+    }, [])
+
+    const subscribe = useCallback((callback: () => void) => {
+      subscribers.current.add(callback)
+      return () => subscribers.current.delete(callback)
+    }, [])
+
+    return {
+      get,
+      set,
+      subscribe
+    }
+  }
+
+  type UseStoreDataReturnType = ReturnType<typeof useStoreData>
+
+  const StoreValueContext = createContext<
+    Omit<UseStoreDataReturnType, 'set'> | undefined
+  >(undefined)
+  const StoreDispatchContext = createContext<
+    UseStoreDataReturnType['set'] | undefined
+  >(undefined)
+
+  const Provider = ({ children }: { children: ReactNode }) => {
+    const { get, set, subscribe } = useStoreData()
+    return (
+      <StoreDispatchContext.Provider value={set}>
+        <StoreValueContext.Provider value={{ get, subscribe }}>
+          {children}
+        </StoreValueContext.Provider>
+      </StoreDispatchContext.Provider>
+    )
+  }
+
+  const useStoreValue = <SelectorOutput,>(
+    selector: (store: Store) => SelectorOutput
+  ): SelectorOutput => {
+    const store = useContext(StoreValueContext)
+    if (store === undefined) {
+      throw new Error(
+        'useStoreValue must be used inside a Provider with a value'
+      )
+    }
+
+    return useSyncExternalStore(
+      store.subscribe,
+      () => selector(store.get()),
+      () => selector(initialState)
+    )
+  }
+
+  const useStoreDispatch = () => {
+    const set = useContext(StoreDispatchContext)
+    if (set === undefined) {
+      throw new Error(
+        'useStoreDispatch must be used inside a Provider with a value'
+      )
+    }
+    return set
+  }
+
+  return {
+    Provider,
+    useStoreValue,
+    useStoreDispatch
+  }
+}
+
+export default generateContext
