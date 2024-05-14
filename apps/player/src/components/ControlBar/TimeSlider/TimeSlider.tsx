15fee3a901c36c5f231a7727a855ac983a6d7719
Dmitriy Yastrebov <somewonderfulguy@gmail.com>
2024-05-14 19:12:08 +0300

    implement timeline - basic version

--- apps/player/src/components/ControlBar/TimeSlider/TimeSlider.tsx
+++ apps/player/src/components/ControlBar/TimeSlider/TimeSlider.tsx
@@ -5,9 +5,11 @@ import styles from './TimeSlider.module.css'
 const TimeSlider = () => {
   return (
     <Slider.Root className={styles.root} defaultValue={[93]} max={194} step={1}>
-      <Slider.Track className={styles.track}>
-        <Slider.Range className={styles.range} />
-      </Slider.Track>
+      <div className={styles.trackContainer}>
+        <Slider.Track className={styles.track}>
+          <Slider.Range className={styles.range} />
+        </Slider.Track>
+      </div>
       <Slider.Thumb className={styles.thumb} aria-label="Time" />
     </Slider.Root>
   )
