9c79fc023f2afe59de66beac54e93e2272f5220a
Dmitriy Yastrebov <somewonderfulguy@gmail.com>
2024-08-23 21:20:12 +0300

    next apps to have src/

--- apps/crypto/.storybook/main.ts
+++ apps/crypto/.storybook/main.ts
@@ -1,30 +1,26 @@
-import type { StorybookConfig } from "@storybook/nextjs";
+import type { StorybookConfig } from '@storybook/nextjs'
 
-import { join, dirname } from "path";
+import { join, dirname } from 'path'
 
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
 function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, "package.json")));
+  return dirname(require.resolve(join(value, 'package.json')))
 }
 const config: StorybookConfig = {
-  stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    getAbsolutePath("@storybook/addon-onboarding"),
-    getAbsolutePath("@storybook/addon-links"),
-    getAbsolutePath("@storybook/addon-essentials"),
-    getAbsolutePath("@chromatic-com/storybook"),
-    getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath('@storybook/addon-links'),
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@chromatic-com/storybook'),
+    getAbsolutePath('@storybook/addon-interactions')
   ],
   framework: {
-    name: getAbsolutePath("@storybook/nextjs"),
-    options: {},
+    name: getAbsolutePath('@storybook/nextjs'),
+    options: {}
   },
-  staticDirs: ["..\\public"],
-};
-export default config;
+  staticDirs: ['..\\public']
+}
+export default config
